65c3ca22f626a39f2c6afa7a087f0b947a29aef9
Leo <leo96pereira@hotmail.com>
2024-10-19 03:14:59 +0100

    First draft at challenge

---
+++ Week-5/S2-Challenge/assets/jss/calculate.js
@@ -0,0 +1,41 @@
+// // Main Equations 
+
+const addition = (num1, num2) => {
+  return num1 + num2;
+};
+
+const subtraction = (num1, num2) => {
+  return num1 - num2;
+};
+
+const division = (num1, num2) => {
+  return num1 / num2;
+};
+
+const multiplication = (num1, num2) => {
+  return num1 * num2;
+};
+
+export { addition, subtraction, division, multiplication };
+
+
+/* Main Calculation */
+
+// function dis(val) {
+//   document.getElementById("display").value += val
+// }
+
+// function appendToDisplay(input) {
+//   display.value += input;
+// }
+
+// function clearDisplay() {
+//   display.value = "";
+// }
+
+// function calculate() {
+//   const expression = display.value;
+//   const result = eval(expression);
+//   display.value = result;
+//   return `${expression} = ${result}`
+// }
