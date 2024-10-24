555a9b9faeac4504f9914a869ac18c0b9b5c639c
Leo <leo96pereira@hotmail.com>
2024-10-24 02:49:19 +0100

    started challenge

--- Week-6/S1 - Challenge/Challenge 2/LeoPereira_W6S1_2_A_assignment.py
+++ Week-6/S1 - Challenge/Challenge 2/LeoPereira_W6S1_2_A_assignment.py
@@ -1,3 +1,27 @@
-# Start writing code with comments form here
+# List
+# Creating and giving value to the list (Array).
+type_animals = ["cat", "dog", "hamster", "fish"]
+print(type_animals)
+
+# Accessing a specific element.
+fav_animal = type_animals[0]
+print(fav_animal)
+
+# Updating an element to a different value.
+type_animals[2] = "platypus"
+print(type_animals)
+
+# Creating a sub-array by slicing.
+least_fav = type_animals[2:4]
+print(least_fav)
+
+# Adding a new element to the end of the array.
+type_animals.append("python")
+print(type_animals)
+
+# Removing an element from the array.
+type_animals.remove("fish")
+print(type_animals)
+
 
 
